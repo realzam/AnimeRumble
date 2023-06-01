f0e0b4b409a441ac1ff7595a2e6b0a4192e4bd2e
realzam <sza0210escom@gmail.com>
2023-06-01 04:17:03 +0000

    update email

--- src/pages/api/user/recovery-password.ts
+++ src/pages/api/user/recovery-password.ts
@@ -3,9 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
 
 import { db } from '@/db';
-import { QuizModel, UserDB, UserModel } from '@/db/models';
+import { UserModel } from '@/db/models';
 import { IQuizBasic } from '@/interfaces';
 import { sendVerifyEmail } from '@/utils/mail';
+import { encodeBase64, singToken } from '@/utils/utils';
 
 type Data =
 	| {
@@ -16,17 +17,34 @@ type Data =
 	| { errors: ValidationError[] };
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
-router.post(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-	// await db.connect();
-	// const quiz = UserModel.findOne({})
-	// await db.disconnect();
-	try {
-		await sendVerifyEmail('sza0210escom@gmail.com');
-		return res.status(200).json({ message: 'ok' });
-	} catch (error) {
-		return res.status(200).json({ message: 'error' });
-	}
-});
+router.post(
+	body('email').isEmail().withMessage('El email es requerido'),
+	async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+		try {
+			const { email } = req.body as { email: string };
+			await db.connect();
+			const user = await UserModel.findOne({ email });
+			await db.disconnect();
+			if (!user) {
+				return res
+					.status(404)
+					.json({ message: 'El correo electrónico no está registrado.' });
+			}
+			const token = await singToken({ userId: user.id }, '30m');
+			const url = `https://anime.real-apps.site/reset-password?token=${encodeBase64(
+				token,
+			)}`;
+			await sendVerifyEmail('sza0210escom@gmail.com', url);
+			return res.status(200).json({ message: 'ok' });
+		} catch (error) {
+			return res.status(200).json({ message: 'error' });
+		}
+	},
+);
 
 export default router.handler({
 	onError: (err, req, res) => {
