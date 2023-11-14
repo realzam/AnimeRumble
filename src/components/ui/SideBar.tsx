2b2a063e35d244b16ad743a4a2b8f2682692ce4a
realzam <sza0210escom@gmail.com>
2023-11-14 06:18:42 +0000

    chore(Root): merge AppDirectory into main 2

--- src/components/ui/SideBar.tsx
+++
@@ -1,5 +0,0 @@
-const SideBar = (): JSX.Element => {
-	return <div>SideBar</div>;
-};
-
-export default SideBar;
--- src/context/ui/UIContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-interface ContextProps {
-	toggleColorMode: () => void;
-}
-
-export const UIContext = createContext({} as ContextProps);
